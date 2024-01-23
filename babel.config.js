72ac7d612d1ee62017d791a5199fda76477edf38
Hồ Đặng Nam Tiến <hodangnamtien@gmail.com>
2024-01-23 21:23:24 +0700

    fix path alias

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,30 @@
-module.exports = 
-  {
-    "presets": ['module:metro-react-native-babel-preset'],
-    "plugins": [
-      
-      [
-        
-        'module-resolver',
-        {
-          root: ['.'],
-          "extensions": [ ".jsx", ".ts", ".tsx",".svg"],
-          alias: {
-            tests: ['./tests/'],
-            "@components": "./src/components/",
-            "@redux": "./src/redux",
-            "@navigation": "./src/navigation/",
-            "@/styles": "./src/styles",
-            "@/icons": "./src/assets/icons/",
-            "@/data": "./src/data/",
-          }
-        }
-      ]
-    ]
-  }
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        // extensions: ['.jsx', '.ts', '.tsx', '.svg'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.ts',
+          '.tsx',
+          '.json',
+          '.svg',
+        ],
+        alias: {
+          tests: ['./tests/'],
+          '@/components': './src/components',
+          '@/redux': './src/redux',
+          '@/navigation': './src/navigation',
+          '@/styles': './src/styles',
+          '@/icons': './src/assets/icons',
+          '@/data': './src/data',
+        },
+      },
+    ],
+  ],
+};
